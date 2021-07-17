b96b41933f771d7050c96258b51447bb7267127f
Lionkk <Lion_yan@outlook.com>
2021-07-17 11:00:55 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,29 @@
-// 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+PlanetX_AILens.initModule()
+PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Color)
+basic.forever(function () {
+    PlanetX_AILens.cameraImage()
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.black)) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.white)) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.blue)) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.green)) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.yellow)) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.red)) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+})
